066fff0bbe677c72676720783f6d95f187642705
sdennett55@gmail.com <sdennett55@gmail.com>
2021-02-06 18:25:23 -0500

    Updates

--- main.js
+++ main.js
@@ -12334,12 +12334,40 @@ class DropZone {
 }
 
 /* harmony default export */ const drop_zone = (DropZone);
+;// CONCATENATED MODULE: ./src/sw.js
+var serviceWorker = () => {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js').then(registration => {
+        console.log('SW registered: ', registration);
+      }).catch(registrationError => {
+        console.log('SW registration failed: ', registrationError);
+      });
+    });
+  }
+  /* TODO: add a button that prompts the PWA install banner-
+    makes it easier to install
+    let deferredPrompt;
+    window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+      const btn = document.getElementById('install');
+    btn.onclick = () => deferredPrompt.prompt();
+    
+    deferredPrompt.userChoice.then((choice) => {
+      console.log(choice);
+    });
+  }); */
+
+};
 ;// CONCATENATED MODULE: ./src/index.js
+
 
 
 
 new drop_zone();
 new classes_form();
+serviceWorker();
 })();
 
 /******/ })()
