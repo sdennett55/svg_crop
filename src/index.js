7ebe83ddafcb03b282ca1850cb0af0f14ff53dcd
Jamie Haywood <jamie.haywood@gmail.com>
2021-02-06 14:19:15 +0000

    feat: add basic SW code for PWA mode

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import DropZone from './classes/drop_zone';
 import Form from './classes/form';
 import './main.css';
+import {serviceWorker} from './sw';
 
 new DropZone();
 new Form();
+serviceWorker();
